b9f2b19360640fd206a90acc2a37e677d90e3c7e
Diorgenes Morais <diorgenesmorais@gmail.com>
2017-03-14 22:47:23 -0300

    commit da classe principal e classe de teste

---
+++ src/main/java/com/diorgenes/matematica/discreta/Math.java
@@ -0,0 +1,52 @@
+package com.diorgenes.matematica.discreta;
+
+/**
+ * The class {@code Math} contains methods for performing basic numeric
+ * operations
+ * 
+ * @author Diorgenes Morais
+ * @version 1.0.0
+ */
+public class Math {
+
+	/**
+	 * N! - fatorial de n.
+	 * 
+	 * @param n
+	 *            um número do conjunto natural
+	 * @return o fatorial de n (N!).
+	 */
+	public static long nFatorial(int n) {
+		if (n == 1) {
+			return n;
+		} else {
+			return n * nFatorial(n - 1);
+		}
+	}
+
+	/**
+	 * Arranjo Simples
+	 * 
+	 * @param n
+	 *            quantos números no arranjo.
+	 * @param p
+	 *            permuta (distinção).
+	 * @return o produto de n! / (n - p)!
+	 */
+	public static long arranjoSimples(int n, int p) {
+		return nFatorial(n) / nFatorial(n - p);
+	}
+
+	/**
+	 * Combinação Simples
+	 * 
+	 * @param n
+	 *            quantos números na combinação.
+	 * @param p
+	 *            permutas na combinação.
+	 * @return o produto de n! / p!(n - p)!
+	 */
+	public static long combinacaoSimples(int n, int p) {
+		return nFatorial(n) / (nFatorial(p) * nFatorial(n - p));
+	}
+}
---
+++ src/test/java/com/diorgenes/matematica/discreta/MathTest.java
@@ -0,0 +1,37 @@
+package com.diorgenes.matematica.discreta;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MathTest {
+
+	@Test
+	public void deveFatoriarUmNumero() throws Exception {
+		long expected = 120L;
+		/**
+		 * O fatorial do número 5
+		 */
+		assertEquals(expected, Math.nFatorial(5));
+	}
+
+	@Test
+	public void deveCalcularArranjoSimples() throws Exception {
+		long expected = 60L;
+		/**
+		 * Com os algarismos 1,2,3,4,5. Quantos números de 3 algarismos
+		 * distintos podem ser formados?
+		 */
+		assertEquals(expected, Math.arranjoSimples(5, 3));
+	}
+
+	@Test
+	public void deveCalcularCombinacaoSimples() throws Exception {
+		long expected = 10L;
+		/**
+		 * Com as pessoas A,B,C,D,E. Quantas comissões de 3 membros podem ser
+		 * formados?
+		 */
+		assertEquals(expected, Math.combinacaoSimples(5, 3));
+	}
+}
