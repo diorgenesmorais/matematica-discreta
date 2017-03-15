7a0a3074656ecbe20f619a50faa81611678e5eff
Diorgenes Morais <diorgenesmorais@gmail.com>
2017-03-15 20:07:25 -0300

    Teste de permutação com elementos repetidos

--- src/test/java/com/diorgenes/matematica/discreta/MathTest.java
+++ src/test/java/com/diorgenes/matematica/discreta/MathTest.java
@@ -9,7 +9,7 @@ public class MathTest {
 	@Test
 	public void deveFatoriarUmNumero() throws Exception {
 		long expected = 120L;
-		/**
+		/*
 		 * O fatorial do número 5
 		 */
 		assertEquals(expected, Math.nFatorial(5));
@@ -18,7 +18,7 @@ public class MathTest {
 	@Test
 	public void deveCalcularArranjoSimples() throws Exception {
 		long expected = 60L;
-		/**
+		/*
 		 * Com os algarismos 1,2,3,4,5. Quantos números de 3 algarismos
 		 * distintos podem ser formados?
 		 */
@@ -28,7 +28,7 @@ public class MathTest {
 	@Test
 	public void deveCalcularCombinacaoSimples() throws Exception {
 		long expected = 10L;
-		/**
+		/*
 		 * Com as pessoas A,B,C,D,E. Quantas comissões de 3 membros podem ser
 		 * formados?
 		 */
@@ -38,10 +38,20 @@ public class MathTest {
 	@Test
 	public void deveCalcularArranjoComRepeticao() throws Exception {
 		long expected = 125L;
-		/**
+		/*
 		 * Considerando um conjunto das vogais, quantas senhas com 3 letras,
 		 * podemos repetir?
 		 */
 		assertEquals(expected, Math.arranjoComRepeticao(5, 3));
+	}
+
+	@Test
+	public void deveCalcularPermutacaoComElementosRepetidos() throws Exception {
+		long expected = 5040L;
+		/*
+		 * Anagrama da palavra: INTERNET (como existe repetição das letras N, T,
+		 * E)
+		 */
+		assertEquals(expected, Math.permutacaoRepetidos(8, 2, 2, 2));
 	}
 }
